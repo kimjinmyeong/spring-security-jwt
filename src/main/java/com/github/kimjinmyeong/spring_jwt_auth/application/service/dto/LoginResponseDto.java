40975b8a2199a62723745c6d5575fc51bf76d0ee
kimjinmyeong <kjmdkdlel@gmail.com>
2024-11-09 20:03:29 +0900

    feat: Swagger 추가

--- src/main/java/com/github/kimjinmyeong/spring_jwt_auth/application/service/dto/LoginResponseDto.java
+++ src/main/java/com/github/kimjinmyeong/spring_jwt_auth/application/service/dto/LoginResponseDto.java
@@ -1,10 +1,12 @@
 package com.github.kimjinmyeong.spring_jwt_auth.application.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class LoginResponseDto {
+    @Schema(description = "The JWT token for authentication")
     private String token;
 }
--- src/main/java/com/github/kimjinmyeong/spring_jwt_auth/presentation/dto/LoginRequestDto.java
+++ src/main/java/com/github/kimjinmyeong/spring_jwt_auth/presentation/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.github.kimjinmyeong.spring_jwt_auth.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -7,9 +8,10 @@ import lombok.Getter;
 public class LoginRequestDto {
 
     @NotBlank
+    @Schema(description = "The username of the user", example = "john_doe")
     private String username;
 
     @NotBlank
+    @Schema(description = "The password of the user", example = "password123")
     private String password;
-
 }
